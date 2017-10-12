e48af47ca1a7fb4289b5c3fd73f0dee46c41285d
Alysson A Costa <alysson.avila.costa@gmail.com>
2017-10-12 23:50:42 +0000

    Primeiras tags de acentos inseridas. Movi o código de traduzir para translate. A função traduzir fica pelos proximos commits para compatibilidade. Alteração da versão 0.1.0 para 0.1.5

--- lib/html2latex.rb
+++ lib/html2latex.rb
@@ -1,8 +1,14 @@
 require "html2latex/version"
 
 module Html2latex
-  	def Html2latex.traduzir(a)
-		tags = {'<b>' => '\textbf{' ,  				#Negrito
+  	def Html2latex.traduzir(text)
+		Html2latex.translate(text)
+	end
+
+	def Html2latex.translate (text)
+		tags = {
+		'<p>' => '',
+		'<b>' => '\textbf{' ,  				#Negrito
 		'<i>' => '\textit{' ,		        		#Itálico
 		'<u>' => '\underline{' ,	        		#Sublinhado
 		'<strong>' => '\textbf{' ,          		#Negritostrong
@@ -15,14 +21,61 @@ module Html2latex
 		'</h1>' => '}',                     		#fechar h1
 		'<h2>' => '\subsection{',           		#h2 - subtítulo
 		'</h2>' => '}',                      		#fechar h2
+		'<h3>' => '\subsubsection{',           		#h3 - subsubtítulo
+		'</h3>' => '}',                      		#fechar h3
 		'<img src="' => '\includegraphics{',		#abrir imagem
-		'" />' => '}'                        		#fechar imagem   
+		'" />' => '}',                        		#fechar imagem   
+		# Acentos
+		'&Aacute;' => 'Á',
+		'&aacute;' => 'á',
+		'&Agrave;' => 'À',
+		'&agrave;' => 'à',
+		'&Acirc;' => 'Â',
+		'&acirc;' => 'â',
+
+		'&Eacute;' => 'É',
+		'&eacute;' => 'é',
+		'&Egrave;' => 'È',
+		'&egrave;' => 'è',
+		'&Ecirc;' => 'Ê',
+		'&ecirc;' => 'ê',
+
+		'&Iacute;' => 'Í',
+		'&iacute;' => 'í',
+		'&Igrave;' => 'Ì',
+		'&igrave;' => 'ì',
+		'&Icirc;' => 'Î',
+		'&icirc;' => 'î',
+
+		'&Oacute;' => 'Ó',
+		'&oacute;' => 'ó',
+		'&Ograve;' => 'Ò',
+		'&ograve;' => 'ò',
+		'&Ocirc;' => 'Ô',
+		'&ocirc;' => 'ô',
+
+		'&Uacute;' => 'Ú',
+		'&uacute;' => 'ú',
+		'&Ugrave;' => 'Ù',
+		'&ugrave;' => 'ù',
+		'&Ucirc;' => 'Û',
+		'&ucirc;' => 'û',
+
+		'&Ccedil;' => 'Ç',
+		'&ccedil;' => 'ç',
+
+		'&Atilde;' => 'Ã',
+		'&atilde;' => 'ã', 
+		'&Otilde;' => 'Õ',
+		'&otilde;' => 'õ',
+		'&Ntilde;' => 'Ñ',
+		'&ntilde;' => 'ñ'
 
 		}
 
-		tags.each { |key, value| a.gsub! key, value }
+		tags.each { |key, value| text.gsub! key, value }
 
 		#Fechamento TAGs
-		a.gsub! /<\/[a-zA-Z>]+/, '}'
+		text.gsub! /<\/[a-zA-Z>]+/, '}'
 	end
 end
