6eb73842d0c7f265118e9ad737683fe90d42824c
Alysson A. Costa <alysson.avila.costa@gmail.com>
2018-01-26 20:39:24 +0000

    Added more tests to add and delete methods

--- test/test_html2latex.rb
+++ test/test_html2latex.rb
@@ -9,4 +9,19 @@ class Html2latexTest < Minitest::Test
   def test_bold
   	assert_equal "\\textbf{}", Html2latex.translate("<b></b>")
   end
+
+  def test_add
+  	hash = {'<foo>' => '\bar{'}
+  	Html2latex.add(hash)
+  	assert_equal "\\bar{}", Html2latex.translate("<foo></foo>")
+  end
+
+  def test_persist_tag
+  	assert_equal "\\bar{Content}", Html2latex.translate("<foo>Content</foo>")
+  end
+
+  def test_delete
+  	Html2latex.delete('<foo>')
+  	assert_equal "<foo></foo>", Html2latex.translate("<foo></foo>")
+  end
 end
